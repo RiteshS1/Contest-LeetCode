a3923be10107054d1d0cb8389d1dfee34d371cde
Ritesh Sharma <119002315+RiteshS1@users.noreply.github.com>
2024-09-29 23:01:10 +0530

    Update and rename all.cpp to #1 LeetCode /all.cpp

---
+++ #1 LeetCode /all.cpp
@@ -0,0 +1,32 @@
+//3304. Find the K-th Character in String Game I
+// class Solution {
+// public:
+//     char kthCharacter(int k) {
+//     string word = "a";
+   
+
+//     for (int i = 0; word.length() < k + 1; i++) {
+//         string genStr = "";
+
+//         // generate the next set of characters based on word
+//         for (int j = 0; j < word.length(); j++) {
+//             genStr += word[j] + 1;
+//         }
+
+//         word.append(genStr);
+//     }
+
+//     return word[k - 1];
+// }
+// };
+
+//3305. Count of Substrings Containing Every Vowel and K Consonants I
+// -------->Not Done
+
+
+//3306. Count of Substrings Containing Every Vowel and K Consonants II
+//---------->Not Done
+
+
+//3307. Find the K-th Character in String Game II
+//---------->Not Done
